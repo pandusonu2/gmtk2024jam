aadd0ace3105f4d1c54a345368c43d57d7da10d4
Gollapudi Vamsi Krishna <pandusonu@google.com>
2024-08-20 18:44:35 +0200

    Pushing all code at once

---
+++ global.gd
@@ -0,0 +1,101 @@
+extends Node
+
+
+# 1-2 5x5
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x o o o o o x x
+# x x o o o o o S x
+# x x o G o o o x x
+# x x o o o o o x x
+# x x o o o o o x x
+# x x x x x x x x x 
+# x x x x x x x x x
+
+# 1-3 7x7
+# x x x x x x x x x 
+# x o o o o o o o x 
+# x o o o o o o o x
+# x G o o o o o o S
+# x o o o o o o o x
+# x o o o o o o o x
+# x o o o o o o o x
+# x o o o o o o o x 
+# x x x x x x x x x
+
+# 2-1 3x3
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x x o o o x x x 
+# x x x o o o x x x 
+# x x x o G S x x x 
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x x x x x x x x
+
+# 2-2 3x3
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x x o o o x x x 
+# x x x G o o x x x 
+# x x x o o S x x x 
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x x x x x x x x
+
+# 2-3 5x5
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x o G o o o x x
+# x x o o o o o x x
+# x x o o o o o x x
+# x x o o o S o x x
+# x x o o o o o x x
+# x x x x x x x x x 
+# x x x x x x x x x
+
+# 2-4 7x7
+# x x x x x x x x x 
+# x o o o o o o o x 
+# x o o G o o o o x
+# x o o o o o o o x
+# x o o o o o o o x
+# x o o o o o o o x
+# x o o o o o S o x
+# x o o o o o o o x 
+# x x x x x x x x x
+
+# 3-1 3x3
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x x o o o x x x 
+# x x x G o S x x x 
+# x x x o o o x x x 
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x x x x x x x x
+
+# 3-2 5x5
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x o o o o o x x
+# x x o G o o o x x
+# x x o o o o o x x
+# x x o S o o o x x
+# x x o o o o o x x
+# x x x x x x x x x 
+# x x x x x x x x x
+
+# 3-3 5x5
+# x x x x x x x x x 
+# x x x x x x x x x 
+# x x o o o o o x x
+# x x o o o o S x x
+# x x o o o o o x x
+# x x o G o o o x x
+# x x o o o o o x x
+# x x x x x x x x x 
+# x x x x x x x x x
